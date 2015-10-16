98d72220d10238638d4570f04ab39081e6ed888b
neolithos <neolithos@outlook.com>
2015-10-16 19:03:52 +0200

    Add: VB.NET test

---
+++ VBtest/Module1.vb
@@ -0,0 +1,24 @@
+﻿Option Strict On
+Imports Neo.IronLua
+
+Module Module1
+
+    Private Sub TestStrict()
+        Using l As Lua = New Lua ' create the lua script engine
+            Dim g As LuaGlobal = l.CreateEnvironment(Of LuaGlobal)() ' create a environment
+            g.DoChunk("a = 'Hallo World!';", "test.lua") ' create a variable in lua
+            Console.WriteLine(g("a").ToString()) ' access a variable in VB
+            g.DoChunk("function add(b) return b + 3; end;", "test.lua") ' create a function in lua
+            Console.WriteLine("Add(3) = {0}", g.CallMember("add", 3)) ' call the function in VB
+        End Using
+    End Sub
+
+    Sub Main()
+        Console.WriteLine("Strict on")
+        TestStrict()
+        Console.WriteLine("Strict off")
+        Module2.TestDynamic()
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ VBtest/Module2.vb
@@ -0,0 +1,15 @@
+﻿Option Strict Off
+
+Imports Neo.IronLua
+
+Module Module2
+    Public Sub TestDynamic()
+        Using l As Lua = New Lua ' create the lua script engine
+            Dim g As Object = l.CreateEnvironment(Of LuaGlobal)() ' create a environment
+            g.dochunk("a = 'Hallo World!';", "test.lua") ' create a variable in lua
+            Console.WriteLine(g.a) ' access a variable in VB
+            g.dochunk("function add(b) return b + 3; end;", "test.lua") ' create a function in lua
+            Console.WriteLine("Add(3) = {0}", g.add(3)) ' call the function in VB
+        End Using
+    End Sub
+End Module
