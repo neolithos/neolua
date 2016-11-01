0428811673557a500ccd725e02b08f6856d6fc8b
neolithos <neolithos@outlook.com>
2016-11-01 10:58:28 +0100

    Chg: Version to 1.2.11

--- NeoLua/Properties/AssemblyInfoGlobal.cs
+++ NeoLua/Properties/AssemblyInfoGlobal.cs
@@ -6,6 +6,6 @@
 [assembly: AssemblyTrademark("")]
 
 [assembly: AssemblyVersion("5.3.0.0")]
-[assembly: AssemblyFileVersion("1.2.10.0")]
+[assembly: AssemblyFileVersion("1.2.11.0")]
 [assembly: AssemblyConfiguration("beta")]
 //[assembly: AssemblyConfiguration("")]
