1adb995f47dab9423fd8a22fc6f525ac4e2e7fa0
neolithos <neolithos@outlook.com>
2020-10-09 15:07:58 +0200

    Fix: Visual Studio Version

--- NeoLua.NuGet/push.ps1
+++ NeoLua.NuGet/push.ps1
@@ -1,1 +1,1 @@
-﻿& 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\msbuild.exe' /t:Restore`;Push /p:Configuration=Release
+﻿& 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe' /t:Restore`;Push /p:Configuration=Release
