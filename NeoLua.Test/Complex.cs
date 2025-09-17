aafc0467b4524081906ff21d653a88e7c3ef89cc
neolithos <neolithos@outlook.com>
2025-09-17 15:07:44 +0200

    Chg: Style

--- NeoLua.Test/Complex.cs
+++ NeoLua.Test/Complex.cs
@@ -10,78 +10,78 @@ using Neo.IronLua;
 
 namespace LuaDLR.Test
 {
-  public class ComplexTestClass
-  {
-    public static T GenericSimple<T>(T i)
-    {
-      return i;
-    } // proc Test
+	public class ComplexTestClass
+	{
+		public static T GenericSimple<T>(T i)
+		{
+			return i;
+		} // proc Test
 
-    public static string GenericSimple(string i)
-    {
-      return i;
-    } // proc Test
-  } // class ComplexTestClass
+		public static string GenericSimple(string i)
+		{
+			return i;
+		} // proc Test
+	} // class ComplexTestClass
 
-  [TestClass]
-  public class ComplexStructures : TestHelper
-  {
-    [TestMethod]
-    public void Generics01()
-    {
-      TestCode(GetLines("Lua.Generics01.lua"), 3);
-    } // proc Generics01
+	[TestClass]
+	public class ComplexStructures : TestHelper
+	{
+		[TestMethod]
+		public void Generics01()
+		{
+			TestCode(GetLines("Lua.Generics01.lua"), 3);
+		} // proc Generics01
 
-    [TestMethod]
-    public void Generics02()
-    {
-      TestCode("return clr.LuaDLR.Test.ComplexTestClass:GenericSimple(3)", 3);
-      TestCode("return clr.LuaDLR.Test.ComplexTestClass:GenericSimple('3')", "3");
-    } // proc Generics02
+		[TestMethod]
+		public void Generics02()
+		{
+			TestCode("return clr.LuaDLR.Test.ComplexTestClass:GenericSimple(3)", 3);
+			TestCode("return clr.LuaDLR.Test.ComplexTestClass:GenericSimple('3')", "3");
+		} // proc Generics02
 
-    private delegate string TestDelegate(int value);
+		private delegate string TestDelegate(int value);
 
-    [TestMethod]
-    public void Delegate01()
-    {
-      using (Lua l = new Lua())
-      {
-        l.PrintExpressionTree = Console.Out;
-        TestDelegate dlg = l.CreateLambda<TestDelegate>("Test", "return value:ToString();");
-        string sR = dlg(3);
-        Assert.IsTrue(sR == "3");
-      }
-    } // proc Delegate01
+		[TestMethod]
+		public void Delegate01()
+		{
+			using (Lua l = new Lua())
+			{
+				l.PrintExpressionTree = Console.Out;
+				TestDelegate dlg = l.CreateLambda<TestDelegate>("Test", "return value:ToString();");
+				string sR = dlg(3);
+				Assert.IsTrue(sR == "3");
+			}
+		} // proc Delegate01
 
-    [TestMethod]
-    public void Delegate02()
-    {
-      using (Lua l = new Lua())
-      {
-        l.PrintExpressionTree = Console.Out;
-        var dlg = l.CreateLambda<Func<int, int, int>>("Test", "return arg1 + arg2");
-        Assert.IsTrue(dlg(1, 2) == 3);
-      }
-    } // proc Delegate02
+		[TestMethod]
+		public void Delegate02()
+		{
+			using (Lua l = new Lua())
+			{
+				l.PrintExpressionTree = Console.Out;
+				var dlg = l.CreateLambda<Func<int, int, int>>("Test", "return arg1 + arg2");
+				Assert.IsTrue(dlg(1, 2) == 3);
+			}
+		} // proc Delegate02
 
-    [TestMethod]
-    public void Delegate03()
-    {
-      using (Lua l = new Lua())
-      {
-        l.PrintExpressionTree = Console.Out;
-        var dlg = l.CreateLambda<Func<int, int, int>>("Test", "return a + b", "a", "b");
-        Assert.IsTrue(dlg(1, 2) == 3);
-      }
-    } // proc Delegate03
+		[TestMethod]
+		public void Delegate03()
+		{
+			using (Lua l = new Lua())
+			{
+				l.PrintExpressionTree = Console.Out;
+				var dlg = l.CreateLambda<Func<int, int, int>>("Test", "return a + b", "a", "b");
+				Assert.IsTrue(dlg(1, 2) == 3);
+			}
+		} // proc Delegate03
 
-    [TestMethod]
-    public void Coroutines01()
-    {
-      TestCode(GetLines("Lua.Coroutines01.lua"));
-    }
+		[TestMethod]
+		public void Coroutines01()
+		{
+			TestCode(GetLines("Lua.Coroutines01.lua"));
+		}
 
-		[DataTestMethod]
+		[TestMethod]
 		[DataRow("return clr.System.String:Format(\"{0}\", values[0]);", "1.1")]
 		[DataRow("return clr.System.String:Format(\"{0}; {1}; {2}; {3}\", values);", "1.1; 2.2; 3.3; 4.4")]
 		[DataRow("return clr.System.String:Format(clr.System.Globalization.CultureInfo.InvariantCulture, \"{0}; {1}; {2}; {3}\", values);", "1.1; 2.2; 3.3; 4.4")]
@@ -112,7 +112,7 @@ namespace LuaDLR.Test
 		public void NumberToString01()
 		{
 			object[] values = new object[] { 1.1 };
-			
+
 			using (Lua l = new Lua())
 			{
 				l.PrintExpressionTree = Console.Out;
@@ -143,7 +143,5 @@ namespace LuaDLR.Test
 				TestResult(g.DoChunk(c), "1 - a", "1 - 2");
 			}
 		}
-
-		
 	} // class ComplexStructures
 }
